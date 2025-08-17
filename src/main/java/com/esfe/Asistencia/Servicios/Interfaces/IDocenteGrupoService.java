51c90b560f4a71c84e70e4800ba134cbe1118c67
Mena <walthermena0799@gmail.com>
2025-08-17 11:10:30 -0600

    Funcionalidad completa para docente grupo

---
+++ src/main/java/com/esfe/Asistencia/Servicios/Interfaces/IDocenteGrupoService.java
@@ -0,0 +1,22 @@
+package com.esfe.Asistencia.Servicios.Interfaces;
+
+import java.util.*;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.esfe.Asistencia.Modelos.DocenteGrupo;
+
+public interface IDocenteGrupoService {
+
+    List<DocenteGrupo> obtenerTodos();
+
+    Page<DocenteGrupo> buscarTodosPaginados(Pageable pageable);
+
+    DocenteGrupo buscarPorId(Integer id);
+
+    DocenteGrupo crearOEditar(DocenteGrupo docenteGrupo);
+
+    void eliminarPorId(Integer id);
+}
